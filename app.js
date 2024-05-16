87cc8c4e5d180088996de5bdc4b72bb4c096daae
Omer Koyuncu <omerkoyuncutr@gmail.com>
2024-05-16 17:10:32 +0300

    course modeli controller, route ve ejs sayfası eklendi

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const pageRouter = require('./routes/pageRoutes');
+const courseRouter = require('./routes/courseRoutes');
 
 const app = express();
+
+//Connect DB
+mongoose
+  .connect('mongodb://127.0.0.1:27017/smartedu-db')
+  .then(() => console.log('DB Connected!'));
 
 //Template Engine
 app.set('view engine', 'ejs');
 
 //Middlewares
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //Routes
 app.use('/', pageRouter);
+app.use('/courses', courseRouter);
 
 const port = 3000;
 app.listen(port, () => {
