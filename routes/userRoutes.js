9365c8a9068d364d5e48c157543014fbb72c32b8
Omer Koyuncu <omerkoyuncutr@gmail.com>
2024-05-19 10:11:48 +0300

    express-validator eklendi login register form validator duzeltme

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,31 @@
 const express = require('express');
+const { body } = require('express-validator');
 const authControllers = require('../controllers/authControllers');
 const authMiddleware = require('../middlewares/authMiddleware');
+const User = require('../models/User');
 
 const router = express.Router();
 
-router.route('/signup').post(authControllers.createUser);
+router.route('/signup').post(
+  [
+    body('name').not().isEmpty().withMessage('Please Enter Your Name'),
+
+    body('email')
+      .isEmail()
+      .withMessage('Please Enter Valid Email')
+      .custom((userEmail) => {
+        return User.findOne({ email: userEmail }).then((user) => {
+          if (user) {
+            return Promise.reject('Email is already exists!');
+          }
+        });
+      }),
+
+    body('password').not().isEmpty().withMessage('Please Enter A Password'),
+  ],
+
+  authControllers.createUser
+);
 router.route('/login').post(authControllers.loginUser);
 router.route('/logout').get(authControllers.logoutUser);
 router
